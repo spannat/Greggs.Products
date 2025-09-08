5ae4f7fb89baeccfa50c3864b1e1790328bff4f8
Anna Thompson <anna.thomp@outlook.com>
2025-09-08 08:44:56 +0100

    removed dup

--- Greggs.Products.Api/Startup.cs
+++ Greggs.Products.Api/Startup.cs
@@ -39,45 +39,4 @@ public class Startup
 
         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
-}using Greggs.Products.Api.DataAccess;
-using Greggs.Products.Api.Models;
-using Greggs.Products.Api.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace Greggs.Products.Api;
-
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddScoped<IDataAccess<Product>, ProductAccess>();
-        services.AddScoped<IProductService, ProductService>();
-        services.AddSingleton<ICurrencyConverter, CurrencyConverter>();
-
-        services.AddControllers();
-
-        services.AddSwaggerGen();
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-
-        app.UseSwagger();
-        app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Greggs Products API V1"); });
-
-        app.UseHttpsRedirection();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-    }
 }
